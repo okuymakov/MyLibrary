d8403bac951ecaf36ee0748487f762cbfa598cdb
okuymakov <oleg.kuymakov@bk.ru>
2023-01-15 19:57:55 +0300

    Add BaseBottomSheetFragment

---
+++ app/src/main/java/com/kuymakov/mylibrary/base/BaseBottomSheetFragment.kt
@@ -0,0 +1,28 @@
+package com.kuymakov.mylibrary.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BaseBottomSheetFragment<T : ViewBinding> : BottomSheetDialogFragment() {
+    abstract val inflater: (LayoutInflater, ViewGroup?, Boolean) -> T
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = inflater(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
